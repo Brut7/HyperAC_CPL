6d27ee94e38b4ba0640f9a14a82d8b122c57ba5f
Owner <Owner@DESKTOP-8LNWXQB>
2023-11-09 08:16:04 +0100

    page allos and checks removed

--- CPL0/drivers.h
+++ CPL0/drivers.h
@@ -8,4 +8,8 @@ NTSTATUS PopulateSystemModules(_Out_ PSYSTEM_MODULES pSystemModules);
 NTSTATUS FindSystemModuleByAddress(_In_ ULONG64 Address, _Out_ PRTL_MODULE_EXTENDED_INFO pSystemModule);
 NTSTATUS FindSystemModuleByName(_In_ CONST CHAR* ModuleName, _Out_ PRTL_MODULE_EXTENDED_INFO pSystemModule);
 
+#pragma alloc_text(PAGE, PopulateSystemModules)
+#pragma alloc_text(PAGE, FindSystemModuleByAddress)
+#pragma alloc_text(PAGE, FindSystemModuleByName)
+
 #endif // H_DRIVERS
--- CPL0/pe.h
+++ CPL0/pe.h
@@ -8,4 +8,8 @@ PIMAGE_NT_HEADERS GetNtHeaders(_In_ ULONG64 Base);
 PIMAGE_EXPORT_DIRECTORY GetExportDirectory(_In_ ULONG64 Base, _In_ PIMAGE_NT_HEADERS Nt);
 ULONG64 FindExport(_In_ ULONG64 Base, _In_ CONST CHAR* Name);
 
+#pragma alloc_text(PAGE, GetNtHeaders)
+#pragma alloc_text(PAGE, GetExportDirectory)
+#pragma alloc_text(PAGE, FindExport)
+
 #endif // H_PE
